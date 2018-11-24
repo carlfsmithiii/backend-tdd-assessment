686b0a5149ff6dc56ec37d3092ce48cd45b075ae
Carl Smith <carlfsmithiii@gmail.com>
2018-11-24 18:26:51 -0500

    added test and implementation for help display -- not currently formatted correctly

--- echo.py
+++ echo.py
@@ -5,12 +5,20 @@
 __author__ = "???"
 
 
+import argparse
 import sys
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    pass
+    parser = argparse.ArgumentParser(
+        description="Perform transformation on input text.")
+    parser.add_argument('text', help="text to be manipulated")
+    parser.add_argument('-u', '--upper', help='convert text to uppercase')
+    parser.add_argument('-l', '--lower', help='convert text to lowercase')
+    parser.add_argument('-t', '--title', help='convert text to titlecase')
+    args = parser.parse_args()
+    return args
 
 
 def main(args):
@@ -19,4 +27,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    pass
+    main(create_parser())
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -2,9 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import subprocess
 import echo
 
 # Your test case class goes here
+
+
+class EchoTest(unittest.TestCase):
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
+
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
 
 
 if __name__ == '__main__':
